d94fccb959a18c90cf0d4d2af6ea033738e58603
Paul <paul.cordon@laposte.net>
2018-04-19 14:11:47 +0200

    ajout search bar

--- DeezerApp/app/src/main/java/com/projet4a/ensim/deezerapplication/MainActivity.java
+++ DeezerApp/app/src/main/java/com/projet4a/ensim/deezerapplication/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
@@ -20,7 +23,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final RecyclerView mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
-
+        final Button searchBtn = (Button) findViewById(R.id.btnSearch);
+        final EditText edtText = (EditText)findViewById(R.id.editTextSearch);
         // use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
         mRecyclerView.setHasFixedSize(true);
@@ -31,21 +35,27 @@ public class MainActivity extends AppCompatActivity {
 
         // specify an adapter (see also next example)
 
-        Response.Listener<DataSearchAuthor> responseListener = new Response.Listener<DataSearchAuthor>() {
+        searchBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onResponse(DataSearchAuthor response) {
-                Log.d(TAG,response.toString());
-                ArtistAdapteur adapteur=new ArtistAdapteur(MainActivity.this,response.getData());
-                mRecyclerView.setAdapter(adapteur);
-            }
-        };
-        Response.ErrorListener errorListener=new Response.ErrorListener(){
+            public void onClick(View v) {
+                Response.Listener<DataSearchAuthor> responseListener = new Response.Listener<DataSearchAuthor>() {
+                    @Override
+                    public void onResponse(DataSearchAuthor response) {
+                        Log.d(TAG,response.toString());
+                        ArtistAdapteur adapteur=new ArtistAdapteur(MainActivity.this,response.getData());
+                        mRecyclerView.setAdapter(adapteur);
+                    }
+                };
+                Response.ErrorListener errorListener=new Response.ErrorListener(){
 
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.d(TAG,"VolleyError");
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d(TAG,"VolleyError");
+                    }
+                };
+                DeezerService.INSTANCE.searchAuthor(edtText.getText().toString(), responseListener, errorListener,MainActivity.this);
             }
-        };
-        DeezerService.INSTANCE.searchAuthor("muse", responseListener, errorListener,this);
+        });
+
     }
 }
